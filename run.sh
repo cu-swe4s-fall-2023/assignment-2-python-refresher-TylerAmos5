901bcfbe4242fefe5a594accfb712187f39442b0
Tyler Amos <tamos5@jhu.edu>
2023-09-21 12:37:51 -0600

    test cases added to run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,20 @@
 set -e
 set -u
 
-echo ..Running print_fires.py..
-python3 print_fires.py
+echo ..Running print_fires.py with good inputs..
+python3 print_fires.py --file_name=Agrofood_co2_emission.csv \
+                       --query_column=0 --query_value="United States of America" \
+                       --result_column=3
+echo Done!
+
+echo ..Running print_fires.py with bad file name..
+python3 print_fires.py --file_name=Agrofood_co2_emission \
+                       --query_column=0 --query_value="United States of America" \
+                       --result_column=3
+echo Done!
+
+echo ..Running print_fires.py with a typecasting error..
+python3 print_fires.py --file_name=Agrofood_co2_emission \
+                       --query_column=0 --query_value="United States of America" \
+                       --result_column=0
 echo Done!
